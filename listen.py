5bf6123f1381fe568b171f43c2f407e2fd868e8f
Alexander <alexander.h.sandstrom@gmail.com>
2023-02-07 13:31:56 +0100

    Comment

--- listen.py
+++ listen.py
@@ -1,6 +1,6 @@
 from pymavlink import mavutil
 
-# Start a connection listening on a UDP port
+# Start a connection listening on port 14445 where QGC forwards messages
 the_connection = mavutil.mavlink_connection('localhost:14445', baud=57600)
 
 print("Waiting for heartbeat")
