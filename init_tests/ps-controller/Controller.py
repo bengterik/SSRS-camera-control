9df2f7a4bd31ef8816a41c4bb9e76fe643b87447
Alexander <alexander.h.sandstrom@gmail.com>
2023-02-23 16:13:07 +0100

    Switched to using deque

--- init_tests/ps-controller/Controller.py
+++ init_tests/ps-controller/Controller.py
@@ -1,21 +1,25 @@
 from threading import Thread
 import time
 import asyncio
+from collections import deque
 import evdev
 from evdev import InputDevice, categorize, ecodes
 
 class ControllerThread(Thread):
 
-    def __init__(self, loop):
+    def __init__(self, loop, DPAD_deque, RST_deque):
         Thread.__init__(self, daemon=True)
         self.loop = loop
+        self.DPAD_deque = DPAD_deque
+        self.RST_deque = RST_deque
+
         self.RST_horiz = 127 # RST = Right Stick
         self.RST_vert = 127
-        self.dev = InputDevice('/dev/input/event15')
         
         self.DPAD_horiz = 0 # DPAD = arrow keys on controller
         self.DPAD_vert = 0
 
+        self.dev = InputDevice('/dev/input/event15')
         devices = [evdev.InputDevice(path) for path in evdev.list_devices()]
         
         print("Device(s):")
@@ -24,7 +28,6 @@ class ControllerThread(Thread):
 
     async def game_controller_listener(self, dev):
         async for ev in dev.async_read_loop():
-            #print("Horiz: %s, Vert: %s" % (self.horiz, self.vert))
             if ev.code == 3:
                 self.RST_horiz = ev.value
 
@@ -36,9 +39,15 @@ class ControllerThread(Thread):
 
             elif ev.code == 17:
                 self.DPAD_vert += ev.value 
+
+            if ev.code == 3 or ev.code == 4: # Joystick input
+                self.RST_deque.appendleft(self.joystick_position()) 
+            elif ev.code == 16 or ev.code == 17: # D-Pad input
+                self.DPAD_deque.appendleft(self.DPAD_position())
+        
     
     def joystick_position(self):
-        return (self.RST_vert, self.RST_horiz)
+        return(self.RST_vert, self.RST_horiz)
     
     def DPAD_position(self):
         temp = (self.DPAD_vert, self.DPAD_horiz)
@@ -59,9 +68,11 @@ class Controller:
     def __init__(self):
         self.pitch = 0
         self.yaw = 0
+        self.DPAD_deque = deque(maxlen=1)
+        self.RST_deque = deque(maxlen=1)
         
-        self.controller_t = ControllerThread(asyncio.get_event_loop())
-        self.controller_t.start() 
+        self.controller_t = ControllerThread(asyncio.get_event_loop(), self.DPAD_deque, self.RST_deque)
+        self.controller_t.start()
 
     def scale(self, value):
         scaled = value - 127
@@ -72,12 +83,20 @@ class Controller:
             return 0
     
     def joystick_read(self):
-        vert, horiz = self.controller_t.joystick_position()
+        try:
+            vert, horiz = self.RST_deque[0] # Take last value
+        except IndexError:
+            vert, horiz = 127, 127
+
         self.pitch -= round(self.scale(vert)*JOYSTICK_PITCH_SENSITIVITY) # inverted on controller
         self.yaw += round(self.scale(horiz)*JOYSTICK_YAW_SENSITIVITY)
     
     def DPAD_read(self):
-        vert, horiz = self.controller_t.DPAD_position()
+        try:
+            vert, horiz = self.DPAD_deque.pop()
+        except IndexError:
+            vert, horiz = 0, 0
+
         self.pitch -= vert # inverted on controller
         self.yaw += horiz
     
