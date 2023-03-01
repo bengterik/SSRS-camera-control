0f0a9e70b75e3cb32e38f48cb8da162c26c802bc
Alexander <alexander.h.sandstrom@gmail.com>
2023-03-01 14:09:24 +0100

    Added css

--- web-app/app.py
+++ web-app/app.py
@@ -1,13 +1,7 @@
-import streamlit as st
-from streamlit_webrtc import webrtc_streamer
-import av
-import cv2
+from flask import Flask, render_template
 
-def callback(frame):
-    img = frame.to_ndarray(format="bgr24")
+app = Flask(__name__)
 
-    img = cv2.cvtColor(cv2.Canny(img, 100, 200), cv2.COLOR_GRAY2BGR)
-
-    return av.VideoFrame.from_ndarray(img, format="bgr24")
-
-webrtc_streamer(key="example", video_frame_callback=callback)
+@app.route('/')
+def index():
+    return render_template('index.html')
