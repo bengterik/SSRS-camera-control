6af647f71fdf36c7939827097c89cca9e4f05a4a
Alexander <alexander.h.sandstrom@gmail.com>
2023-03-02 15:16:40 +0100

    Simplification of controller class

--- web-app/Controller.py
+++ web-app/Controller.py
@@ -1,5 +1,3 @@
-import Connection
-from HandController import HandController
 # Software limits to saturate keyboard input (DOES NOT AFFECT HARDWARE LIMITS)
 YAW_MIN = -240
 YAW_MAX = -116
@@ -10,18 +8,12 @@ PITCH_MAX = 30
 PITCH_NEUTRAL = 6
 PITCH_RETRACTED = 6
 
-DEGREE_PER_KEY_PRESS = 1
-
 class Controller:
-    def __init__(self, input_device):
-        self.controller = input_device
-        self.connection = Connection.Connection()
-
-        self.pitch = PITCH_NEUTRAL # neutral pitch
-        self.yaw = YAW_NEUTRAL # neutral yaw
-        self.rate = 12 # degrees per key press
-
-        self.last_pos = (self.pitch, self.yaw)
+    """
+    Holds the state of the gimbal and manages saturation of control input
+    """
+    def __init__(self, start_pos = (PITCH_NEUTRAL, YAW_NEUTRAL)):
+        self.last_pos = start_pos
 
     def add_sat(self, value, change, min, max):
             if value <= min:
@@ -32,11 +24,8 @@ class Controller:
                 return value + change
 
     def update_position(self, pitch, yaw):
-        new_pos = (self.add_sat(self.pitch, pitch, PITCH_MIN, PITCH_MAX), 
-                   self.add_sat(self.yaw, yaw, YAW_MIN, YAW_MAX))
+        new_pitch = self.add_sat(self.last_pos[0], pitch, PITCH_MIN, PITCH_MAX), 
+        new_yaw = self.add_sat(self.last_pos[1], yaw, YAW_MIN, YAW_MAX)
+        last_pos = (new_pitch, new_yaw)
 
-        
-        if new_pos != self.last_pos:
-            self.connection.gimbal_pitch_yaw(new_pos[0], new_pos[1])
-            
-            self.last_pos = new_pos
+        return last_pos
