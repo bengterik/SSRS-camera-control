ced317e07e7d12857df749675e80573befcbc7cb
Alexander <alexander.h.sandstrom@gmail.com>
2023-03-06 12:20:15 +0100

    Replace the old RTC script

---
+++ web-app/static/js/getJanusStream.js
@@ -0,0 +1,7 @@
+Janus.init({
+    debug: true,
+    dependencies: Janus.useDefaultDependencies(), // or: Janus.useOldDependencies() to get the behaviour of previous Janus versions
+    callback: function() {
+            // Done!
+    }
+ });
