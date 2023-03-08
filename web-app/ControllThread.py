0195a91dcd61ade0683c33ea36fa743911fe9189
Alexander <alexander.h.sandstrom@gmail.com>
2023-03-08 12:19:39 +0100

    Only send when position is changed

--- web-app/ControllThread.py
+++ web-app/ControllThread.py
@@ -7,13 +7,17 @@ class ControllThread:
     def __init__(self):
         self.controller = Controller()
         self.connection = Connection()
+        self.last_pitch_yaw = (0, 0)
+        
         self.thread = threading.Thread(target=self.run)
         self.thread.start()
 
     def run(self):
         while True:
             pitch, yaw = self.controller.pitch_yaw
-            self.connection.gimbal_pitch_yaw(pitch, yaw)
+            if((pitch, yaw) != self.last_pitch_yaw):
+                self.connection.gimbal_pitch_yaw(pitch, yaw)
+                self.last_pitch_yaw = (pitch, yaw)        
             time.sleep(0.05)
 
     def update_position(self, yaw, pitch):
